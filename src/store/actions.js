a1274fda949c9addaaae83e889536362fa890b86
zackmore <zackmoredev@gmail.com>
2017-03-27 19:24:41 +0800

    rewrite create node mutations

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,6 +3,27 @@ import _ from 'lodash'
 
 export default {
   CREATE_NODE (context, { node, followingId=null }) {
-    context.commit('CREATE_NODE', { node, followingId })
+    console.log(node)
+    switch (node.type) {
+      case 'start':
+        context.commit('CREATE_START_NODE', { node })
+        break
+      case 'normal':
+        context.commit('CREATE_NORMAL_NODE', { node, followingId })
+        break
+      case 'end':
+        context.commit('CREATE_END_NODE', { node, followingId })
+        break
+      case 'question':
+        context.commit('CREATE_QUESTION_NODE', { node, followingId })
+        break
+      case 'answer':
+        break
+      default:
+        break
+    }
+
+
+    // context.commit('CREATE_NODE', { node, followingId })
   }
 }
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,6 +2,7 @@
 import _ from 'lodash'
 
 export default {
+  /*
   CREATE_NODE (state, { node, followingId }) {
     console.log(followingId)
     console.log(node)
@@ -10,7 +11,6 @@ export default {
       state.nodes.push(node)
     }
 
-    /*
     if (_.isNull(followingId)) {
       // Usually for init put a start node
       state.nodes.push(node)
@@ -21,6 +21,34 @@ export default {
       state.nodes.splice(index, 1, following)
       state.nodes.push(node)
     }
-    */
+  }
+  */
+
+  CREATE_START_NODE (state, { node }) {
+    state.nodes.push(node)
+  },
+
+  CREATE_NORMAL_NODE (state, { node, followingId }) {
+    let following = _.find(state.nodes, { id: followingId })
+    let index = _.findIndex(state.nodes, following)
+    following.next = node.id
+    state.nodes.splice(index, 1, following)
+    state.nodes.push(node)
+  },
+
+  CREATE_END_NODE (state, { node, followingId }) {
+    let following = _.find(state.nodes, { id: followingId })
+    let index = _.findIndex(state.nodes, following)
+    following.next = node.id
+    state.nodes.splice(index, 1, following)
+    state.nodes.push(node)
+  },
+
+  CREATE_QUESTION_NODE (state, { node, followingId }) {
+    let following = _.find(state.nodes, { id: followingId })
+    let index = _.findIndex(state.nodes, following)
+    following.next = node.id
+    state.nodes.splice(index, 1, following)
+    state.nodes.push(node)
   }
 }
