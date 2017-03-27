6f449a3254636f8c3a5195283d62a365c5b376d2
zackmore <zackmoredev@gmail.com>
2017-03-27 11:05:27 +0800

    new node tmpl

--- src/components/nodes/AddNext.vue
+++ src/components/nodes/AddNext.vue
@@ -1,5 +1,25 @@
 <template>
 <div class="node-next">
-add
+<button @click="showUp">add next</button>
+
+<div :style="{ display: show }">
+ok
+</div>
 </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      show: 'none'
+    }
+  },
+
+  methods: {
+    showUp () {
+      this.show = 'block'
+    }
+  }
+}
+</script>
